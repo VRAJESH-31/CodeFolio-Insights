8b9ea257b547abdd4755b13d648b687a6e03b36c
VRAJESH N PANDYA <npandyavrajesh31@gmail.com>
2025-09-19 12:45:10 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <button class="bg-sky-500 hover:bg-sky-700 ...">Save changes</button>
+    </div>
+  )
+}
+
+export default App
