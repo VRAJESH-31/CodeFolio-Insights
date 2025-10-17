4c5762e71af27e6106b6fc31b6ccb2aeb0092eb3
Ashok-Bhatt <ashokbhatt2048@gmail.com>
2025-10-18 00:18:08 +0530

    feat: Add functionality to get and update user coding profile links

---
+++ backend/src/models/profiles.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema({
+    userId : {
+        type: mongoose.Types.ObjectId,
+        ref: "users",
+        require: true,
+    },
+    githubUsername : {
+        type: String,
+        default: "",
+    },
+    leetCodeUsername: {
+        type: String,
+        default: "",
+    },
+    gfgUsername: {
+        type: String,
+        default: "",
+    },
+    interviewbitUsername: {
+        type: String,
+        default: "",
+    },
+    codechefUsername: {
+        type: String,
+        default: "",
+    }
+});
+
+const ProfileModel = mongoose.model('profiles', ProfileSchema);
+
+export default ProfileModel;
